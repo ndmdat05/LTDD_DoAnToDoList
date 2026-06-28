3886ed0b3442880def37a3b34a5582e6674de2cf
phanhongphuc-sys <23130249@st.hcmuaf.edu.vn>
2026-06-29 00:57:41 +0700

    ms làm sơ add project

--- app/src/main/java/com/example/myapplication/Task.java
+++ app/src/main/java/com/example/myapplication/Task.java
@@ -3,18 +3,29 @@ package com.example.myapplication;
 public class Task {
     private String category;
     private String title;
+    private String content;
     private int progress;
     private String status;
+    private String startDate;
+    private String endDate;
 
-    public Task(String category, String title, int progress, String status) {
+    public Task() {
+    }
+    public Task(String category, String title, String content, int progress, String status, String startDate, String endDate) {
         this.category = category;
         this.title = title;
+        this.content = content;
         this.progress = progress;
         this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     public String getCategory() { return category; }
     public String getTitle() { return title; }
+    public String getContent() { return content; }
     public int getProgress() { return progress; }
     public String getStatus() { return status; }
+    public String getStartDate() { return startDate; }
+    public String getEndDate() { return endDate; }
 }
